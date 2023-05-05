f3af93c3bfb9fcfa4bd61072b434c24f6bc9133f
agu-am <agustin.cs96@gmail.com>
2023-05-05 15:12:50 -0300

    ejercicio-3b

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const resultado = fetch("https://reqres.in/api/users?per_page=12")
+
+resultado
+    .then(respuesta => respuesta.json())
+    .then(respuestaJSON => {
+        respuestaJSON.data.forEach(
+            persona => {
+                const card = document.createRange().createContextualFragment(`
+                <article>
+                <div class="container-img">
+                    <img src="${persona.avatar}">
+                </div>
+                <div class="container-info">
+                <p class="nombre">${persona.first_name} ${persona.last_name}</p>
+                <p class="titulo">${persona.email}</p>
+                <button class="btn"><i class="icon-btn"></i>Add me</button>
+                </div>
+                </article>
+                `);
+                const container = document.querySelector("#container")
+                container.append(card);
+            }
+        )
+    })
